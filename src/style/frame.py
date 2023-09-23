6637f2f047f1c400403e8e6065e9a3729071d292
Ozay Akcan <mail@ozayakcan.com.tr>
2023-09-23 12:03:31 +0300

    Default frame now returning itself.

--- src/style/frame.py
+++ src/style/frame.py
@@ -5,3 +5,4 @@ def set_frame_attrs(fr: Frame, mainFrame: Frame):
     fr.grid(column=0, row=0, sticky='nesw')
     #fr.grid_rowconfigure(1, weight=1)
     #fr.grid_columnconfigure(1, weight=1)
+    return fr
